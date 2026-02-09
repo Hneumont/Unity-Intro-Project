a0e2b831af7d15921f5e62e466c582b5d3cd0333
Hneumont <hberumengovea@student.neumont.edu>
2026-02-09 16:05:17 -0700

    Inventory start
    lot of scripts are missing or broken

---
+++ Assets/Common/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+//using Systems.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //[SerializeField] List<iItem> items = new List<iItem>();
+
+    //public iItem CurrentItem { get; private set; } = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //CurrentItem = items[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
